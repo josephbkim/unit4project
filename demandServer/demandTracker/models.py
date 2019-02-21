4962dc291e236199172a53919b99535c2327a1ba
joe kim <j80n89@gmail.com>
2019-02-21 12:20:56 -0500

    model 1 State

--- demandServer/demandTracker/models.py
+++ demandServer/demandTracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class State(models.Model):
+    name = models.Charfield(max_length=25)
+
+    def __str__(self):
+        return self.name
