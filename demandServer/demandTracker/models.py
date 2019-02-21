daf58e67f762f9efc71b9cf78a4ad2bf2f282b44
joe kim <j80n89@gmail.com>
2019-02-21 12:32:36 -0500

    model 2 sites

--- demandServer/demandTracker/models.py
+++ demandServer/demandTracker/models.py
@@ -8,3 +8,15 @@ class State(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Sites(models.Model):
+    name = models.Charfield(max_length=50)
+    numDeliveries = models.IntegerField(max_length=50)
+    totPurAmnt = models.DecimalField(
+        max_digits=50, decimal_places=2, default=0.00)
+    state = models.ForeignKey(State, on_delete=models.CASCADE,
+                              blank=True, null=True, related_name="sites")
+
+    def __str__(self):
+        return self.name
