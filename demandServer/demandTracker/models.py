99e33762bd22f06cb0011c80526367f7f3d840eb
joe kim <j80n89@gmail.com>
2019-02-21 13:10:37 -0500

    models 4 and 5 complete

--- demandServer/demandTracker/models.py
+++ demandServer/demandTracker/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
 
 class State(models.Model):
-    name = models.Charfield(max_length=25)
+    name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
 
 
 class Sites(models.Model):
-    name = models.Charfield(max_length=50)
+    name = models.CharField(max_length=50)
     numDeliveries = models.IntegerField(max_length=50)
     totPurAmnt = models.DecimalField(
         max_digits=50, decimal_places=2, default=0.00)
@@ -23,11 +24,36 @@ class Sites(models.Model):
 
 
 class Stores(models.Model):
-    name = models.Charfield(max_length=25)
-    location = models.Charfield(max_length=50)
+    name = models.CharField(max_length=25)
+    location = models.CharField(max_length=50)
     numDeliveries = models.IntegerField(max_length=50)
     state = models.ForeignKey(State, on_delete=models.CASCADE,
                               blank=True, null=True, related_name="stores")
+
+    def __str__(self):
+        return self.name
+
+
+class Deliveries(models.Model):
+    date = models.DateField(("Date"), default=date.today)
+    delAddress = models.CharField(max_length=50)
+    purAmount = models.DecimalField(
+        max_digits=50, decimal_places=2, default=0.00)
+    store = models.ForeignKey(
+        Stores, on_delete=models.CASCADE, blank=True, null=True, related_name="deliveries")
+
+    def __str__(self):
+        return self.date
+
+
+class Customers(models.Model):
+    name = models.CharField(max_length=25)
+    address = models.CharField(max_length=50)
+    custLong = models.DecimalField(max_digits=9, decimal_places=6)
+    custLat = models.DecimalField(max_digits=9, decimal_places=6)
+    phoneNum = models.CharField(max_digits=10)
+    deliveries = models.ForeignKey(
+        Deliveries, on_delete=models.CASCADE, blank=True, null=True, related_name="customers")
 
     def __str__(self):
         return self.name
