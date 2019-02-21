2e43e32821e18f59e9a5f0cf92d8bf2adc75afe9
joe kim <j80n89@gmail.com>
2019-02-21 15:16:38 -0500

    admin setups admin.py

--- demandServer/demandTracker/admin.py
+++ demandServer/demandTracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(State)
+admin.site.register(Sites)
+admin.site.register(Stores)
+admin.site.register(Deliveries)
+admin.site.register(Customers)
