5fe51a53ac9da312935681ffa94125ff55244d4b
joe kim <j80n89@gmail.com>
2019-02-22 11:04:49 -0500

    moving to the front end

--- demandServer/demandTracker/admin.py
+++ demandServer/demandTracker/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-<<<<<<< HEAD
-# Register your models here.
-
-=======
->>>>>>> d6e27679c2c0d9a838f6c18ed7a1fbb17e498418
 admin.site.register(State)
 admin.site.register(Site)
 admin.site.register(Store)
