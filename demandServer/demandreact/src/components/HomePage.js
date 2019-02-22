1c950d726e75f6167e59a5ad46c5e1786deec5fd
joe kim <j80n89@gmail.com>
2019-02-22 16:06:33 -0500

    home app nav site storelis component creation

---
+++ demandServer/demandreact/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <div>This is the homepage!</div>
+        <div>
+          <h1>Welcome</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ demandServer/demandreact/src/components/SitesListPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SitesListPage extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default SitesListPage;
