61ddd867d0e44f1772c816a975c1fb6a78774ba4
joe kim <j80n89@gmail.com>
2019-02-24 13:17:28 -0500

    store storelist basic setup

---
+++ demandServer/demandreact/src/components/Store.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class Store extends Component {
+  render() {
+    return <StoreBox />;
+  }
+}
+
+export default Store;
+const StoreBox = styled.div`
+  justify-self: center;
+  height: 30vh;
+  width: 60vw;
+  background-color: black;
+  margin: 2px;
+`;
--- demandServer/demandreact/src/components/StoreListPage.js
+++ demandServer/demandreact/src/components/StoreListPage.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import Store from "./Store";
+import styled from "styled-components";
 
 class StoreListPage extends Component {
   render() {
-    return <div>This is the Store List Page</div>;
+    return (
+      <ConDiv>
+        <h1>This is the Store List Page</h1>
+        <StoreDiv>
+          <Store />
+        </StoreDiv>
+      </ConDiv>
+    );
   }
 }
 
 export default StoreListPage;
+
+const ConDiv = styled.div`
+  display: block;
+  text-align: center;
+  justify-items: center;
+  height: 100vh;
+  margin: auto;
+`;
+
+const StoreDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: space-around;
+  height: 100vh;
+  width: 100vw;
+`;
