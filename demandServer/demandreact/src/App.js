573dde172849c5257a79c4f9b6428e9d42c0a739
joe kim <j80n89@gmail.com>
2019-02-24 11:22:05 -0500

    app.js initial routes

--- demandServer/demandreact/src/App.js
+++ demandServer/demandreact/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
+import StoreListPage from "./components/StoreListPage";
+import SitesListPage from "./components/SitesListPage";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <HomePage />
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/stores" component={StoreListPage} />
+            <Route exact path="/sites" component={SitesListPage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
